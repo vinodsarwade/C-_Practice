c296ccdf54c0e43802005b93b9a9c754a1138bde
vinodsarwade <vinodsarwade7@gmail.com>
2023-10-04 22:18:03 +0530

    by me

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int firstNumber;
+   int secondNumber;
+   cout<<"enter numbers: "<<endl;
+   cin>>firstNumber;
+   cout<<"you entered: "<<firstNumber<<" now enter second number !!"<<endl;
+   cin>>secondNumber;
+   cout<<"second Number:"<<secondNumber;
+}
