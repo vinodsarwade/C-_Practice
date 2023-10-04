986138efcc6ec864befb33670c68daa32bd67d09
vinodsarwade <vinodsarwade7@gmail.com>
2023-10-04 22:39:05 +0530

    vinod

---
+++ inhertence.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class employee
+{
+    public:
+    void show()
+    {
+        cout<<"vinod is my name";
+    }
+};
+
+int main()
+{
+    employee e1;
+    e1.show();
+}
